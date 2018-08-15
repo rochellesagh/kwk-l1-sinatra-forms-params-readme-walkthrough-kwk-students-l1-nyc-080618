1fea18cef4764991450449a58239c611ef3874d7
rochellesagh <rsrochelli@gmail.com>
2018-08-15 18:04:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -8,4 +8,23 @@ class App < Sinatra::Base
 
   # Add your post route and action below
 
+
+ get '/' do
+    erb :fortune
+  end
+
+
+ post '/results' do
+  @name = params[:name]
+  @sign = params[:sign]
+  @food = params[:food]
+  @been_before = params[:been_before]
+    erb :results
+  end
+   post '/food_form' do
+  @food = params[:food]
+      erb :food_form
+    end 
 end
+
+
